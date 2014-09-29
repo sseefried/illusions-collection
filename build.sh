7a22286a403c4e3b3cd87d11b97f57bc330ab1bd
Sean Seefried <sean.seefried@gmail.com>
2014-09-29 23:15:38 +1000

    Modified build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -f cabal.sandbox.config
 rm -rf .cabal-sandbox
 cabal sandbox init
-cabal install -j gtk2hs-buildtools
+cabal install -j gtk2hs-buildtools random-1.0.1.1
 cabal install -j --with-gcc=gcc-4.8
